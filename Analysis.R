198ccd68dbaad07e3cdd166a2f4dbfc22f32a084
Matias Braccini <Matias.Braccini@fish.wa.gov.au>
2019-08-07 16:20:12 +0800

    just added a bit of code for reading in data and manipulating it

--- Analysis.R
+++ Analysis.R
@@ -1,14 +1,29 @@
   #---------- SCRIPT FOR ANALYSIS ACOUSTIC TAGGING DATA -------#
 
+library(dplyr)
+library(lubridate)
 
 # Section 1. read in data -------------------------------------------------
+setwd("M:\\Fisheries Research\\FinFish\\Shark\\Bronze whaler\\Data")
+Detections=read.csv("Detections.csv",stringsAsFactors=F)
 
 
 
 # Section 2. Input parameters -------------------------------------------------
 
 
+# Section 3. Data manipulation section -------------------------------------------------
+  #convert some variables to date, time, and remove duplicates
+Detections=Detections%>%mutate(ReleaseLatitude=-abs(ReleaseLatitude),
+                               Latitude=-abs(Latitude),
+                               ReleaseDate=ymd(ReleaseDate),
+                               DateTime.local=ymd_hms(DateTime.local),
+                               Date.local=ymd(Date.local),
+                               Time.local=hms(Time.local))%>%
+                        distinct(TagCode,DateTime.local,Species,Sex,Latitude,Longitude,.keep_all=T)
 
-# Section 3. Procedure section -------------------------------------------------
+
+
+# Section 4. Procedure section -------------------------------------------------
 
 
